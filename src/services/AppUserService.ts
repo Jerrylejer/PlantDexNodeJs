ce9614d1d0bd21659ba50c2ed72634183e1f0df3
Jerrylejer <jerrylejer23@yahoo.fr>
2024-03-26 14:48:07 +0100

    Ajout des services pour les entities Plant et AppUser => les services vont se charger de déclencher les requêtes du repository selon les req faites par le controller

---
+++ src/services/AppUserService.ts
@@ -0,0 +1,31 @@
+import AppDataSource from "../data-source";
+import { AppUser } from "../entities/appUser";
+
+
+class AppUserService {
+    // Je créé mon repository qui va communiquer avec ma BDD via des reqêtes pré-définies en SQL
+    private appUserRepository = AppDataSource.getRepository(AppUser);
+    
+    // Je créé mes appels service qui déclencheront les requêtes CRUD à la BDD
+    // Listes de tous les users
+    async getAllAppUsers() {
+        return await this.appUserRepository.find();
+    }
+    // Un user
+    async getAppUserById(id: number) {
+        return await this.appUserRepository.findOneBy({id:id});
+    }
+    // Ajouter un user 
+    async createAppUser(user: AppUser) {
+        const newUser = this.appUserRepository.create(user);
+        return await this.appUserRepository.save(newUser);
+    }
+    // Supprimer un user
+    async deleteAppUser(id: string) { 
+        return await this.appUserRepository.delete(id);
+    }
+    // Modifier un user
+    async updateAppUser(id: number, user: AppUser) {
+        return await this.appUserRepository.update(id, user);
+    }
+}
---
+++ src/services/PlantService.ts
@@ -0,0 +1,31 @@
+import AppDataSource from "../data-source";
+import { Plant } from "../entities/Plant";
+
+
+class PlantService {
+    // Je créé mon repository qui va communiquer avec ma BDD via des reqêtes pré-définies en SQL
+    private plantRepository = AppDataSource.getRepository(Plant);
+    
+    // Je créé mes appels service qui déclencheront les requêtes CRUD à la BDD
+    // Listes de toutes les plantes
+    async getAllPlants() {
+        return await this.plantRepository.find();
+    }
+    // Une plante
+    async getPlantById(id: number) {
+        return await this.plantRepository.findOneBy({id:id});
+    }
+    // Ajouter une plante 
+    async createPlant(plant: Plant) {
+        const newPlant = this.plantRepository.create(plant);
+        return await this.plantRepository.save(newPlant);
+    }
+    // Supprimer une plante
+    async deletePlant(id: string) { 
+        return await this.plantRepository.delete(id);
+    }
+    // Modifier une plante
+    async updatePlant(id: number, plant: Plant) {
+        return await this.plantRepository.update(id, plant);
+    }
+}
