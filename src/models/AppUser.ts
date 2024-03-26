fec58f8fdfee44b7b6ba1bd389d0e8f786c72b03
Jerrylejer <jerrylejer23@yahoo.fr>
2024-03-26 11:22:17 +0100

    Ajout des interfaces models pour mes 2 entities

---
+++ src/models/AppUser.ts
@@ -0,0 +1,7 @@
+interface AppUser {
+    id: number,
+    username: string,
+    email: string,
+    pwd: string
+}
+export default AppUser;
---
+++ src/models/Plant.ts
@@ -0,0 +1,9 @@
+interface Plant {
+    id: number,
+    nom: string,
+    soleil: string,
+    arrosage: number,
+    categorie: string,
+    image: string
+}
+export default Plant;
